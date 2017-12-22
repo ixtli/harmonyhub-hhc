8ad996e0c267b24f2b77c8de8a1ff5b3b693e535
ixtli <chris.galardi@risingtidegames.com>
2017-12-22 16:29:50 -0500

    start discovery

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const discover = new (require("harmonyhubjs-discover"))(61991);
+const HarmonyHubDiscover = require("harmonyhubjs-discover");
+const discover = new HarmonyHubDiscover(61991);
+
+discover.on('online', function (hub) {
+	console.log('discovered', hub.ip);
+});
+
+discover.on('offline', function (hub) {
+	console.log('lost', hub.ip);
+});
+
+discover.on('update', function (hubs) {
+	const knownIPs = hubs.reduce((prev, hub) => {
+		return prev + (prev.length > 0 ? ', ' : '') + hub.ip
+	}, '');
+	console.log("known ips", knownIPs);
+});
 
 discover.start();
